250bb2c54f95ca7b45c4e59f557dee13baea2cac
Lzw-github21 <91644263+Lzw-github21@users.noreply.github.com>
2021-11-04 17:20:37 +0800

    代码同步

---
+++ authorization_del/src/main/java/com/example/demo/eneity/User.java
@@ -0,0 +1,56 @@
+package com.example.demo.eneity;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @author lizhiwei
+ * @date 2021/11/4 16:26
+ */
+public class User implements UserDetails, Serializable {
+
+    private int id;
+    private String username;
+    private String password;
+    private List<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
+
